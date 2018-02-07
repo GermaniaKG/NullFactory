f4c1da4362a0d6d2e3a4917596a8ee367bc687c2
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2018-02-07 17:16:37 +0100

    First commit

---
+++ src/NullFactory.php
@@ -0,0 +1,30 @@
+<?php
+namespace Germania\NullFactory;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
+
+class NullFactory
+{
+
+    use LoggerAwareTrait;
+
+    public $default_return = null;
+
+    public function __construct( $default_return = null, LoggerInterface $logger = null )
+    {
+        $this->setLogger( $logger ?: new NullLogger );
+    }
+
+
+    public function __invoke()
+    {
+        $this->logger->debug("NullFactory", [
+            'num_args' => func_num_args(),
+            'return_value' => $this->default_return
+        ]);
+
+        return $this->default_return;
+    }
+}
